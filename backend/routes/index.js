ad5c357a628fc71d899ac94794e269d912384f9a
Clark <johnclarkmondero@gmail.com>
2025-09-15 20:55:57 +0800

    chore: hotels fetch query, add to routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import hotels from "./hotels.js";
 
 const router = express.Router();
 
+router.use("/hotels", hotels);
+
 // test endpoint
-router.get("/ping", (req, res) => {
-  res.json({ message: "Test" });
-});
+// router.get("/ping", (req, res) => {
+//   res.json({ message: "Test" });
+// });
 
 export default router;
